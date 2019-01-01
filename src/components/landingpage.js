f104e82d9083a05b95d287ae0fbb3e37d9900699
Alaamadhoun <alaafm@live.com>
2019-01-01 21:10:04 +0800

    Landing Page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
   render() {
-    return <h1>Landing</h1>;
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="http://segen.in/wp-content/uploads/2015/10/career.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1> Full Stack Web Developer</h1>
+              <hr />
+              <p>
+                HTML/CSS | Bootstrap | JavaScript | React | React Native |
+                NodeJS
+              </p>
+
+              <div className="social-links">
+                {/* Linkedin */}
+                <a
+                  href="http://google.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/* Github */}
+                <a
+                  href="http://google.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
   }
 }
 
--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,21 @@
 import React from "react";
+
+import About from "./aboutme";
+import Projects from "./projects";
+import Resume from "./resume";
+import Contact from "./contact";
 import LandingPage from "./landingpage";
+
 import { Switch, Route } from "react-router-dom";
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
+    <Route exact path="/aboutme" component={About} />
+    <Route exact path="/contact" component={Contact} />
+    <Route exact path="/projects" component={Projects} />
+    <Route exact path="/resume" component={Resume} />
+    <Route exact path="/contact" component={Contact} />
   </Switch>
 );
 
